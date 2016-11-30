cdd0df523d078d1136dce1222fae7a861844c949
Elia Fagioli <elia136@live.it>
2016-11-30 13:03:22 +0100

    Versione finale.

---
+++ fork_1.c
@@ -0,0 +1,12 @@
+/*
+Elia Fagioli 4 INA
+*/
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+main(){
+    pid_t pid;
+    printf("sono il figlio (PID%d);",getpid());
+getchar();
+getchar();
+}
