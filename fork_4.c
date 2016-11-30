89956146c600f76d2c5137207810c49d425b1631
Elia Fagioli <elia136@live.it>
2016-11-30 13:11:40 +0100

    Versione finale

--- fork_4.c
+++ fork_4.c
@@ -4,23 +4,27 @@ Elia Fagioli 4INA
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/types.h>
+#include<stdlib.h>
+#include<sys/wait.h>
 main(){
-   pid_t pid;
-   int k=13;
+   pid_t pid,wpid;
+   int i;
+   int status;
    printf("sono il padre %d \n",getpid());
    pid=fork();
    if(pid==0){
         printf("figlio PID %d, Il pid di mio padre è %d \n",getpid(),getppid());
-        printf("\n  k=%d ora è %d  \n",k,k*2);
-        k=k*2;
-        printf("\n ora k è %d",k);
+        for(i=10;i>0;i--){
+            printf("%d\n",i);
+            sleep(1);
         }
-   else if(pid>0){
+   }else {if(pid>0){
                 printf("sono il padre %d, ho creato %d",getpid(),pid);
-                printf("\n k=%d ora è %d",k,k*k);
-                k=k*k;
-                printf("\n ora k è = %d",k);
-}
+                   }
+   }
+   wpid=wait(&status);
+   printf("il processo di mio figlio è terminato\n");
+   exit(1);
    getchar();
    getchar();
 }
